1dc5225b92d28f42a12864a372f0d7ef2c078fba
CopterDoctor <Jordananders@me.com>
2018-06-11 13:32:17 -0700

    add responses

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
        })
        .on('response', function (response) {
          console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message: ', response.statusMessage);
+         console.log('Response content type: ', response.headers['content-type']);
        })
-       .pipe(fs.createWriteStream('/Users/jordan/lighthouse/w2d1/Scripting-HTTP-Requests-with-the-Request-Package/future.jpg'), console.log('writing file'));
+       .pipe(fs.createWriteStream('./future.jpg'), console.log('writing file'));
+       console.log('COMPLETE');
+       
